3c2937deac3d757b11a8ffdaf7b7c856e620bfa0
anhphan <anh.phan156@protonmail.com>
2024-07-18 12:00:44 -0400

    vga

--- src/main.rs
+++ src/main.rs
@@ -2,18 +2,11 @@
 #![no_main]
 use core::panic::PanicInfo;
 
-static MSG: &[u8] = b"Hello World!";
+mod vga_buffer;
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    let vga_buffer = 0xb8000 as *mut u8;
-
-    for (i, &byte) in MSG.iter().enumerate() {
-        unsafe {
-            *vga_buffer.offset(i as isize * 2) = byte;
-            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
-        }
-    }
+    vga_buffer::print_sth();
 
     loop {}
 }
